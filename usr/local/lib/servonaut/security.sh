7e7e16da110b231af108123004c20f427c794943
1CAMOU <mgmt.camou@gmail.com>
2024-10-22 21:48:51 +0200

    feat: automatic security updates

---
+++ usr/local/lib/servonaut/security.sh
@@ -0,0 +1,33 @@
+#!/bin/bash
+
+setup_automatic_security_updates() {
+    echo -e "🛡️ Setting up automatic security updates...\n"
+
+    # Install unattended-upgrades package
+    apt-get install -y unattended-upgrades
+
+    # Configure unattended-upgrades
+    cat >/etc/apt/apt.conf.d/50unattended-upgrades <<EOF
+Unattended-Upgrade::Allowed-Origins {
+    "\${distro_id}:\${distro_codename}";
+    "\${distro_id}:\${distro_codename}-security";
+    "\${distro_id}ESMApps:\${distro_codename}-apps-security";
+    "\${distro_id}ESM:\${distro_codename}-infra-security";
+};
+Unattended-Upgrade::Package-Blacklist {
+};
+Unattended-Upgrade::AutoFixInterruptedDpkg "true";
+Unattended-Upgrade::MinimalSteps "true";
+Unattended-Upgrade::InstallOnShutdown "false";
+Unattended-Upgrade::Remove-Unused-Dependencies "true";
+Unattended-Upgrade::Automatic-Reboot "false";
+EOF
+
+    # Enable automatic updates
+    echo 'APT::Periodic::Update-Package-Lists "1";
+APT::Periodic::Download-Upgradeable-Packages "1";
+APT::Periodic::AutocleanInterval "7";
+APT::Periodic::Unattended-Upgrade "1";' >/etc/apt/apt.conf.d/20auto-upgrades
+
+    echo -e "✅ Automatic security updates have been set up successfully.\n"
+}
